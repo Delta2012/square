988881699f9ed39a715a4619e1e2cb2c2693b039
Jiang <jiang@Foxconn.com>
2019-05-20 21:40:17 +0800

    vue-cli init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<style lang='scss' scoped>
+#root {
+	width: 100%;
+	line-height: 100px;
+	font-size: 16px;
+	color:black;
+	text-align: center;
+}
+.loading-area {
+	width: 100%;
+	height: 500px;
+	position: relative;
+}
+</style>
+
+<template>
+	<div id="root">
+		
+		square
+
+		<button @click="showLoading = !showLoading">click me, show loading</button>
+
+		<div class="loading-area">
+			<sq-loading :show="showLoading"></sq-loading>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'root',
+	data() {
+		return {
+			showLoading: false
+		}
+	},
+	computed: {
+		
+	},
+	mounted() {
+		
+	},
+	destroyed() {
+		
+	},
+	methods: {
+		
+	}
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+/**
+ * created by Jason on 2019-5-20
+ * 
+ * see https://cli.vuejs.org/zh/config/ for documentation.
+ * 
+ * vue.config.js 是一个可选的配置文件，
+ * 如果项目的 (和 package.json 同级的) 根目录中存在这个文件，那么它会被 @vue/cli-service 自动加载。
+ */
+
+module.exports = {
+    // 部署应用包时的基本 URL
+    publicPath: '/',
+
+    // 开发模式
+    devServer: {
+        // 指定本地代理端口号
+        port: 9012,
+        // display both warnings and errors on the browser overlay
+        overlay: {
+            warnings: true,
+            errors: true
+        },
+        // 自动打开默认浏览器
+        open: true,
+    },
+};
+
